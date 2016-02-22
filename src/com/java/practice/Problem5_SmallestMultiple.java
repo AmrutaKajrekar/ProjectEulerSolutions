57648c941fd4d41c6a637c5c9804e33aa0ee1c38
Amruta Kajrekar <AmrutaKajrekar@users.noreply.github.com>
2016-02-22 15:47:05 -0800

    Update Problem5_SmallestMultiple.java
    updated problem and my solution

--- src/com/java/practice/Problem5_SmallestMultiple.java
+++ src/com/java/practice/Problem5_SmallestMultiple.java
@@ -7,8 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author amruta
- *
+ * @author Amruta Kajrekar
+ * Problem: 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. 
+ * 	    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ * MySolution: 232792560
  */
 public class Problem5_SmallestMultiple {
 	static List<Integer> array = new ArrayList<Integer>();
